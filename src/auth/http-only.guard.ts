3850fcd8f58f12e02f370759061d2327ebf65574
Telman Gadimov <70106877+telman03@users.noreply.github.com>
2025-02-25 15:12:47 +0400

    Update http-only.guard.ts

--- src/auth/http-only.guard.ts
+++ src/auth/http-only.guard.ts
@@ -3,16 +3,11 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class HttpOnlyGuard implements CanActivate {
-    // This method will be called by the NestJS framework to determine if the user has access to the route
     canActivate(context: ExecutionContext): boolean {
-            // Get the request object
             const request = context.switchToHttp().getRequest();
-            // Check if the cookie is present
             if (request.cookies && 'token' in request.cookies && request.cookies.token.length > 0) {
-            // The cookie is present
             return true;
         }
-        // The cookie is not present, deny access
         return false;
     }
 }
