d18650a66ea99ed1dd652a947a1a3ba4744d63a3
Telman Gadimov <70106877+telman03@users.noreply.github.com>
2025-02-25 15:13:30 +0400

    Update jwt.strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,7 +7,6 @@ import { UUID } from "crypto";
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
-        // The JWT token will be extracted from the cookie
         super({
           jwtFromRequest: ExtractJwt.fromExtractors([
             JwtStrategy.extractJWT,
@@ -17,7 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         });
       }
       private static extractJWT(req: Request): string | null {
-        // Check if the cookie is present
         if (req.cookies && 'token' in req.cookies) {
           return req.cookies.token;
         }
